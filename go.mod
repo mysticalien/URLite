9370930fa00ef5f97af074d3994d2acb3a3b0321
Celiac Signus <celiacsi@il-f5.msk.21-school.ru>
2024-08-29 16:19:01 +0300

    Initial commit

---
+++ go.mod
@@ -0,0 +1,1 @@
+module url-shortener
---
+++ internal/config/config.go
@@ -0,0 +1,34 @@
+package config
+
+import (
+	"log"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Env         string `yaml:"env" env-default:"local"`
+	StoragePath string `yaml:"storage_path" env-required:"true"`
+	HTTPServer  `yaml:"http_server"`
+}
+
+type HTTPServer struct {
+	Address     string        `yaml:"address" env-default:"localhost:8080"`
+	Timeout     time.Duration `yaml:"timeout" env-default:"4s"`
+	IdleTimeout time.Duration `yaml:"idle_timeout" env-default:"60s"`
+}
+
+func MustLoad() {
+	// panic("not implemented")
+	configPath := os.Getenv("CONFIG_PATH")
+	if configPath == "" {
+		log.Fatal("CONFIG_PATH isn't set")
+	}
+
+	// check if file exists
+	if _, err := os.Stat(configPath); os.IsNotExist(err) {
+		log.Fatalf("config file does not exist: %s", configPath)
+	}
+
+	var cfg Config
+}
